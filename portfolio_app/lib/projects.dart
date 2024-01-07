ff3f1183b1395907b1312c5a0cecba10874d6c54
Lokesh Kumar <lokeshkmr995@gmail.com>
2024-01-07 14:01:07 +0530

    changes in project section

--- portfolio_app/lib/projects.dart
+++ portfolio_app/lib/projects.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, unused_import
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Projects extends StatefulWidget {
   const Projects({super.key});
@@ -12,73 +13,80 @@ class Projects extends StatefulWidget {
 
 class _ProjectsState extends State<Projects> {
 
-  mypro(lang,title,desc,star){    return   Container(
-                  height: 220,
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: Card(
-                    color: Color(0xff262628),
-                    child: Container(
-                      margin: EdgeInsets.only(left: 20, right: 20, top: 30),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            lang,
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                          SizedBox(
-                    height: 15,
-
-                  ), 
-                          Text(
-                            title,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold),
-                          ),
-                            SizedBox(
-                    height: 3,
-
+  mypro(lang, title, desc, star, githubLink) {
+    return Container(
+      height: 220,
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: Card(
+        color: Color(0xff262628),
+        child: Container(
+          margin: EdgeInsets.only(left: 20, right: 20, top: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                lang,
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Text(
+                desc,
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.white70,
+                    size: 10,
                   ),
-                          Text(
-                            desc,
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                          SizedBox(
-                    height: 10,
-
+                  Text(
+                    star,
+                    style: TextStyle(color: Colors.white70),
                   ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                color: Colors.white70,
-                                size: 10,
-                              ),
-                              Text(
-                            star,
-                            style: TextStyle(color: Colors.white70),
-                          ),
-                              SizedBox(
+                  SizedBox(
                     width: 4,
-
                   ),
                   Expanded(child: Container()),
-                              IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    FontAwesomeIcons.github,
-                                    color: Colors.white,
-                                  ))
-                            ],
-                          )
-                        ],
-                      ),
+                  GestureDetector(
+                    onTap: () {
+                      launch(githubLink);
+                    },
+                    child: Row(
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.github,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'GitHub',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
                     ),
                   ),
-                );
-             
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
   @override
   Widget build(BuildContext context) {
@@ -97,9 +105,9 @@ class _ProjectsState extends State<Projects> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                mypro('FLUTTER', 'Portfolio App', 'Skill & Specialization', '5'),
-                mypro('PYTHON', 'Application', 'SIGN LANGUAGE RECOGNITION', '5'),
-                mypro('FLUTTER', 'Clone App', 'Instagram clone ', '5'),
+                 mypro('FLUTTER', 'Portfolio App', 'Skill & Specialization', '5', "https://github.com/Lokesh002cse/portfolioapp"),
+                 mypro('PYTHON', 'Application', 'SIGN LANGUAGE RECOGNITION', '5', "https://github.com/Lokesh002cse/sign-language-recognition-app"),
+                 mypro('FLUTTER', 'Clone App', 'Instagram clone ', '5', "https://github.com/Lokesh002cse/instagram-clone"),
                 
               ]
             )),
